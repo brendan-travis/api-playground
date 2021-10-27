88ed1b758a8f0f13700957dfe74e4152d6760b2f
Brendan Travis <brendantravis1@gmail.com>
2021-10-27 07:44:25 +0100

    Refactor api project naming from kebab-case to PascalCase + add missing test project

--- api/src/ApiPlayground.Api/Program.cs
+++ api/src/ApiPlayground.Api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace api_playground.api
+namespace ApiPlayground.Api
 {
     public class Program
     {
